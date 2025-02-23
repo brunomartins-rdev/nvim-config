46ee5262fb01e2e406425d267985153ff4ed5a92
brunomartins-rdev <bruno.silva.martins@hotmail.com>
2025-02-23 00:21:49 +0000

    added lsp for python, r, html, css, typescript and lua

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,7 +1,7 @@
 -- Catppuccing theme
 return {
-  'catppuccin/nvim', 
-  name = 'catppuccin', 
+  'catppuccin/nvim',
+  name = 'catppuccin',
   priority = 1000,
   config = function ()
     require("catppuccin").setup({
--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,4 +1,25 @@
 -- Base LSP support
 return {
-  'neovim/nvim-lspconfig',
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim"
+  },
+  config = function()
+    local lspconfig = require("lspconfig")
+
+    -- Python
+    lspconfig.pyright.setup({})
+
+    -- R
+    lspconfig.r_language_server.setup({})
+
+    -- Add more LSPS to configure them here
+    lspconfig.cssls.setup({})
+    lspconfig.html.setup({})
+    lspconfig.lua_ls.setup({})
+    lspconfig.ts_ls.setup({})
+
+  end,
 }
+
