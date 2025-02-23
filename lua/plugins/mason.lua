94a7ffe93bca4c557a1fc5f7cb898f1aaa91342d
brunomartins-rdev <bruno.silva.martins@hotmail.com>
2025-02-23 00:39:36 +0000

    added requirements in mason

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,7 +1,19 @@
 -- LSP manager
 return {
   "williamboman/mason.nvim",
+  dependencies = { "williamboman/mason-lspconfig.nvim" },
   config = function()
-    require('mason').setup()
-  end
+    require("mason").setup()
+    require("mason-lspconfig").setup({
+      ensure_installed = {
+        "pyright",
+        "r_language_server",
+        "html",
+        "cssls",
+        "ts_ls",
+        "lua_ls",
+      },
+    })
+  end,
 }
+
